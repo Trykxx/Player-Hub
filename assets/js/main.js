4d132fc436094976c4ed4f3c3a8ef6c640717f82
Trykxx <amitnicolas@gmail.com>
2024-04-25 16:54:56 +0200

    forum fini et connexion

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,17 @@
-const convTarget = document.querySelector('#user-conv')
-console.log(convTarget);
-const convAffich = document.querySelector("#msg-container")
-console.log(convAffich);
-convTarget.addEventListener('click', function(){
-convAffich.style.backgroundColor = 'blue'
+//* messages.html
+// const convTarget = document.querySelector('#user-conv')
+// console.log(convTarget);
+// const convAffich = document.querySelector("#msg-container")
+// console.log(convAffich);
+// convTarget.addEventListener('click', function(){
+// convAffich.style.backgroundColor = 'blue'
+// })
+
+const connexionBtnTarget = document.querySelector("#connexion-btn");
+const divConnexionTarget = document.querySelector("#connexion-container")
+
+connexionBtnTarget.addEventListener('click', function(e){
+    e.preventDefault()
+    divConnexionTarget.classList.toggle('visible')
 })
+
