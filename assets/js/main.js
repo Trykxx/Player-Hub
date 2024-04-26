899424c52234a57352ca69c0424a82c1f3deb224
Trykxx <amitnicolas@gmail.com>
2024-04-26 15:02:46 +0200

    modif aprem

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,12 @@
-//* messages.html
-// const convTarget = document.querySelector('#user-conv')
-// console.log(convTarget);
-// const convAffich = document.querySelector("#msg-container")
-// console.log(convAffich);
-// convTarget.addEventListener('click', function(){
-// convAffich.style.backgroundColor = 'blue'
-// })
-
+// TODO rendre fonctionnel sur toutes les pages autres que index.html
 const connexionBtnTarget = document.querySelector("#connexion-btn");
 const divConnexionTarget = document.querySelector("#connexion-container")
+const mainContent = document.querySelector("#main-content");
 
 connexionBtnTarget.addEventListener('click', function(e){
     e.preventDefault()
     divConnexionTarget.classList.toggle('visible')
     divConnexionTarget.classList.toggle('hidden');
+    mainContent.classList.toggle('blur');
 })
 
