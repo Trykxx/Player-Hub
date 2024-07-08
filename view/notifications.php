bcad8e9f5998d270c894304abce24537164e4639
Trykxx <amitnicolas@gmail.com>
2024-07-08 10:53:14 +0200

    modifs

--- view/notifications.php
+++ view/notifications.php
@@ -1,6 +1,6 @@
 <?php
 $styleCss = '../../../player-hub/assets/css/style.css';
-$pageCss = '../../../player-hub/assets/css/test.css';
+$pageCss = '../../../player-hub/assets/css/notifications.css';
 
 $pageJs = '../../../player-hub/assets/js/main.js';
 
@@ -14,7 +14,6 @@ require '/xampp/htdocs/player-hub/src/templates/header.php';
         <span>Mentions</span>
         <span>Forums</span>
       </div>
-      <h3>test</h3>
     </section>
   </main>
 
